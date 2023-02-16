14ca56cb79d213b8741c2825e2851c5662c6d120
Candace Eckels <candace.eckels@gmail.com>
2023-02-15 19:16:06 -0700

    grocery list warm up

--- warm_up/grocery_list_array.rb
+++ warm_up/grocery_list_array.rb
@@ -7,5 +7,7 @@ fridge_items_1 = ["milk", "eggs", "eggs","eggs", "eggs",
 
 # 1. What is problematic about listing `fridge_items_1`
 # as an array structured grocery list?
+  # - should count the number of items rather than repeating them
 
 # 2. What might be a more readable way to structure this?
+  # - a hash values for the item to keep track of all the items
